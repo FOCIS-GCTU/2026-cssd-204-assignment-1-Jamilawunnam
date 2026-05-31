867381241e4575c75c9abecfc00ae65ca24e6ca8
Jamilawunnam <fadlurrahmanjamilatu@gmail.com>
2026-05-31 23:07:59 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description:Print out my initials.
+# Assignment Number: 1
+# 
+# Name:Fadlurrahman Jamilatu Wunnam
+# STUDENT ID:2425402299
+# Email: fadlurrahmanjamilatu@gmail.com
+# Grader: Jamila
+#
+# On my honor, Fadlurrahman Jamilatu, this programming assignment is my own work 
+# and i have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in large stylized letters
 
+    print()
+    print("...FJW")
+    print()
+
+    print("...FFFFFFFFFFFF............JJJJJJJJJJJJ............W..........W....")
+    print("...FFFFFFFFFFFF..................JJ................W..........W....")
+    print("...FF............................JJ................W..........W....")
+    print("...FFFFFFFFFF....................JJ................W....WW....W....")
+    print("...FF............................JJ................W....WW....W....")
+    print("...FF............................JJ................W...W..W...W....")
+    print("...FF............................JJ................W...W..W...W....")
+    print("...FF................JJ..........JJ................W..W....W..W....")
+    print("...FF................JJJJJJJJJJJJJJ....**..........W..W.W..W..W**")
+    print("...FF..................JJJJJJJJJJ......**..........W..WW...W.WW**")
+
+    print()
 
 
 main()
